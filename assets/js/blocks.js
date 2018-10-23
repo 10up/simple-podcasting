8f4319b107782447074df2838de19d775e9bc9c4
Ryan Welcher <ryan.welcher@10up.com>
2018-10-23 15:36:56 -0400

    Adds the supports object to stop multiple blocks from being inserted

--- assets/js/blocks.js
+++ assets/js/blocks.js
@@ -36,8 +36,9 @@ export default registerBlockType(
 		description: __( 'Insert a podcast episode into a post. To add it to a podcast feed, select a podcast in document settings.', 'simple-podcasting' ),
 		category: 'common',
 		icon: 'microphone',
-		useOnce: true,
- 
+		supports: {
+			multiple: false,
+		},
 		attributes: {
 			id: {
 				type: 'number',
@@ -98,7 +99,7 @@ export default registerBlockType(
 					className,
 				};
 			}
-		
+
 			render() {
 				const { id, align, caption, podcastTerm, captioned, explicit, url, mime, duration } = this.props.attributes;
 				const { setAttributes, isSelected } = this.props;
