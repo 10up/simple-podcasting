1c3f5b8f4e5eb1d4e7a9b748a8a92bb4e75fd8fb
Ryan Welcher <ryan.welcher@10up.com>
2018-11-09 15:10:55 -0500

    Outputs the new email field and wraps it and the itunes:author node in a itunes:owner node

--- includes/customize-feed.php
+++ includes/customize-feed.php
@@ -80,11 +80,21 @@ function feed_head() {
 		echo '<itunes:summary>' . esc_html( wp_strip_all_tags( $summary ) ) . "</itunes:summary>\n";
 	}
 
-	$author = get_term_meta( $term->term_id, 'podcasting_talent_name', true );
 
+	echo '<itunes:owner>';
+
+	$author = get_term_meta( $term->term_id, 'podcasting_talent_name', true );
 	if ( ! empty( $author ) ) {
 		echo '<itunes:author>' . esc_html( wp_strip_all_tags( $author ) ) . "</itunes:author>\n";
 	}
+
+	$podcasting_email = get_term_meta( $term->term_id, 'podcasting_email', true );
+	$email            = ! empty( $podcasting_email ) ? $podcasting_email : get_bloginfo( 'admin_email' );
+	if ( ! empty( $email ) ) {
+		echo '<itunes:email>' . esc_html( wp_strip_all_tags( $email ) ) . "</itunes:email>\n";
+	}
+
+	echo '</itunes:owner>';
 
 	$copyright = get_term_meta( $term->term_id, 'podcasting_copyright', true );
 
