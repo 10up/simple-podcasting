dd27f0488b136c4d0c798ef2ac68e138f1baf541
Helen Hou-Sandi <helen.y.hou@gmail.com>
2018-12-04 09:54:32 -0600

    Version bumps

--- readme.txt
+++ readme.txt
@@ -6,7 +6,7 @@ Tags: podcasting, gutenberg, gutenberg-ready, gutenberg-blocks, blocks
 Requires at least: 4.6
 Tested up to: 4.9.6
 Requires PHP: 5.3
-Stable tag: 1.0.1
+Stable tag: 1.1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: podcasting
--- simple-podcasting.php
+++ simple-podcasting.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://wordpress.org/plugins/simple-podcasting
  * Description: Easily set up multiple podcast feeds using built-in WordPress posts. Includes beta Gutenberg support.
  * Author: 10up
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author URI: http://10up.com/
  *
  * @package tenup_podcasting
@@ -12,7 +12,7 @@
 
 namespace tenup_podcasting;
 
-define( 'PODCASTING_VERSION', '1.0.1' );
+define( 'PODCASTING_VERSION', '1.1.0' );
 define( 'PODCASTING_PATH', dirname( __FILE__ ) . '/' );
 define( 'PODCASTING_URL', plugin_dir_url( __FILE__ ) );
 define( 'TAXONOMY_NAME', 'podcasting_podcasts' );
