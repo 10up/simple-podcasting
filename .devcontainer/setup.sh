9991b728e0029c99f94b810d68fce02546c422a6
Tung Du <dinhtungdu@gmail.com>
2020-06-29 17:29:55 +0700

    build: add vscode remote container support

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,30 @@
+#!  /bin/bash
+
+#Site configuration options
+SITE_TITLE="Simple Podcasting Development Site"
+ADMIN_USER=admin
+ADMIN_PASS=password
+ADMIN_EMAIL="admin@example.com"
+
+echo "Setting up WordPress"
+
+sudo chown www-data: -R /var/www/html
+
+cd /var/www/html
+
+rm -f wp-config.php
+
+wp core download --force
+wp config create --dbhost="db" --dbname="wordpress" --dbuser="admin" --dbpass="password" --skip-check
+wp db reset --yes
+wp core install --url="http://localhost:8080" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_email="$ADMIN_EMAIL" --admin_password="$ADMIN_PASS" --skip-email
+wp plugin activate simple-podcasting
+
+echo "Install plugin dependencies"
+
+cd /var/www/html/wp-content/plugins/simple-podcasting
+composer install
+npm install 10
+nvm use 10
+npm install
+npm run build
