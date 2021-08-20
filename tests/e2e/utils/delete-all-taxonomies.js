3e78e8b9cf3478b5fa1250d91f925d19958bf881
Tung Du <dinhtungdu@gmail.com>
2021-08-20 15:46:39 +0700

    fix

--- tests/e2e/utils/delete-all-taxonomies.js
+++ tests/e2e/utils/delete-all-taxonomies.js
@@ -5,7 +5,9 @@ import { visitAdminPage } from '@wordpress/e2e-test-utils';
 
 export async function deleteAllTaxonomies() {
 	await visitAdminPage('edit-tags.php', 'taxonomy=podcasting_podcasts&podcasts=true');
+	await page.waitForSelector('.wp-list-table.tags');
 	await expect(page).toClick('#cb-select-all-1');
 	await expect(page).toSelect('#bulk-action-selector-top', 'Delete');
 	await expect(page).toClick('#doaction');
+	return page.waitForSelector('.wp-list-table.tags');
 }
