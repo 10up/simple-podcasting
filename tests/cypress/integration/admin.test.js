d13488ea73be0cac370238a02522540397e8041b
Tung Du <dinhtungdu@gmail.com>
2021-08-27 17:43:13 +0700

    fix: remove conditional test

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -1,11 +1,7 @@
 describe( 'Admin can login and make sure plugin is activated', () => {
 	it( 'Can activate plugin if it is deactivated', () => {
 		cy.visitAdminPage( 'plugins.php' );
-		cy.get( 'body' ).then( ( $body ) => {
-			if ( $body.find( '#deactivate-simple-podcasting' ).length > 0 ) {
-				cy.get( '#deactivate-simple-podcasting' ).click();
-			}
-		} );
+		cy.get( '#deactivate-simple-podcasting' ).click();
 		cy.get( '#activate-simple-podcasting' ).click();
 		cy.get( '#deactivate-simple-podcasting' ).should( 'be.visible' );
 	} );
