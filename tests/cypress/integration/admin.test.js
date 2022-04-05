e71d571604622e1dc4fdf8c6c3bbc3df3481fcd7
Tung Du <dinhtungdu@gmail.com>
2022-04-05 22:08:37 +0700

    replace custom commands by cypress-wp-utils

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -1,8 +1,12 @@
-describe( 'Admin can login and make sure plugin is activated', () => {
-	it( 'Can activate plugin if it is deactivated', () => {
-		cy.visitAdminPage( 'plugins.php' );
-		cy.get( '#deactivate-simple-podcasting' ).click();
-		cy.get( '#activate-simple-podcasting' ).click();
-		cy.get( '#deactivate-simple-podcasting' ).should( 'be.visible' );
-	} );
-} );
+describe('Admin can login and make sure plugin is activated', () => {
+	before(() => {
+		cy.login();
+	});
+
+	it('Can activate plugin if it is deactivated', () => {
+		cy.visit('/wp-admin/plugins.php');
+		cy.get('#deactivate-simple-podcasting').click();
+		cy.get('#activate-simple-podcasting').click();
+		cy.get('#deactivate-simple-podcasting').should('be.visible');
+	});
+});
