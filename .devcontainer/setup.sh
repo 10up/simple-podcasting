546e067adffae003280f93137aa981abb360fb24
Tung Du <dinhtungdu@gmail.com>
2022-07-08 11:29:08 +0700

    use wordpress image only

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -18,6 +18,8 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>setup.log 2>&1
 
+touch /root/.bashrc
+
 # Prepare a nice name from project name for the site title.
 function getTitleFromSlug()
 {
@@ -27,7 +29,25 @@ function getTitleFromSlug()
     echo "${___slug[@]^}"
 }
 
-source ~/.bashrc
+apt-get update && \
+apt-get upgrade -y && \
+apt-get install -y git && \
+apt-get install -y sudo && \
+apt-get install -y zip
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Install WP CLI
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+        chmod +x wp-cli.phar && \
+        mv wp-cli.phar /usr/local/bin/wp
+
+echo 'alias wp="wp --allow-root"' >> /root/.bashrc
+
+# Install nvm and node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && nvm install --lts
 
 # Install dependencies
 cd /var/www/html/wp-content/${PROJECT_TYPE}s/${SLUG}/
@@ -37,10 +57,15 @@ composer i
 # Install WordPress and activate the plugin/theme.
 cd /var/www/html/
 echo "Setting up WordPress at $SITE_HOST"
-wp db reset --yes
-wp core install --url="$SITE_HOST" --title="$(getTitleFromSlug) Development" --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
+wp --allow-root db reset --yes
+wp --allow-root core install --url="$SITE_HOST" --title="$(getTitleFromSlug) Development" --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
 
 echo "Activate $SLUG"
-wp $PROJECT_TYPE activate $SLUG
+wp --allow-root $PROJECT_TYPE activate $SLUG
+
+# Install Xdebug
+pecl install "xdebug" || true && docker-php-ext-enable xdebug
+echo "xdebug.mode=debug" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+echo "xdebug.start_with_request=yes" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 
 exit 0
