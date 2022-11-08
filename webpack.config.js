4316317b82675dcadc2685e1dfa25aa6b4fc903b
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-11-08 08:01:45 +0300

    Lint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const CopyPlugin = require( 'copy-webpack-plugin' );
-const path = require( 'path' );
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    ...defaultConfig,
+	...defaultConfig,
 	output: {
 		...defaultConfig.output,
-		path: path.resolve( process.cwd(), 'dist' ),
+		path: path.resolve(process.cwd(), 'dist'),
 	},
-    entry: {
-        'blocks': path.resolve( process.cwd(), 'assets/js', 'blocks.js' ),
-        'podcasting-edit-post': path.resolve( process.cwd(), 'assets/js', 'podcasting-edit-post.js' ),
-        'podcasting-edit-term': path.resolve( process.cwd(), 'assets/js', 'podcasting-edit-term.js' ),
-        'podcasting-onboarding': path.resolve( process.cwd(), 'assets/js', 'onboarding.js' ),
-    },
-    plugins: [
-        ...defaultConfig.plugins,
-        new CopyPlugin({
-            patterns: [
-              { from: "assets/images/*", to: "./" },
-            ],
-        } )
-    ]
+	entry: {
+		blocks: path.resolve(process.cwd(), 'assets/js', 'blocks.js'),
+		'podcasting-edit-post': path.resolve(
+			process.cwd(),
+			'assets/js',
+			'podcasting-edit-post.js'
+		),
+		'podcasting-edit-term': path.resolve(
+			process.cwd(),
+			'assets/js',
+			'podcasting-edit-term.js'
+		),
+		'podcasting-onboarding': path.resolve(
+			process.cwd(),
+			'assets/js',
+			'onboarding.js'
+		),
+	},
+	plugins: [
+		...defaultConfig.plugins,
+		new CopyPlugin({
+			patterns: [{ from: 'assets/images/*', to: './' }],
+		}),
+	],
 };
