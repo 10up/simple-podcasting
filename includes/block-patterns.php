e3d0119cd4ea5fc4509d71d20acb4597f701b2be
Mehul Gohil <mehul.gohil@get10up.com>
2023-01-21 08:39:23 +0530

    improve description

--- includes/block-patterns.php
+++ includes/block-patterns.php
@@ -16,12 +16,12 @@ function init() {
 		'podcasting/podcast-grid',
 		array(
 			'title'       => __( 'Podcast Grid', 'simple-podcasting' ),
-			'description' => _x( 'This is podcast grid.', 'Block pattern description', 'simple-podcasting' ),
+			'description' => _x( 'Podcast Grid', 'This block pattern is used to display podcast in a grid structure.', 'simple-podcasting' ),
 			'content'     => '<!-- wp:query {"queryId":14,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false},"displayLayout":{"type":"flex","columns":3}} -->
 			<div class="wp-block-query"><!-- wp:post-template -->
 			<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"}},"color":{"background":"#ededed"}},"layout":{"inherit":false}} -->
 			<div class="wp-block-group has-background" style="background-color:#ededed;padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:post-title {"isLink":true,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"1.6rem"}}} /-->
-			
+
 			<!-- wp:post-date {"displayType":"modified","style":{"typography":{"fontSize":"0.8rem"},"spacing":{"margin":{"top":"10px","right":"0px","bottom":"0px","left":"0px"}}}} /--></div>
 			<!-- /wp:group -->
 			<!-- /wp:post-template --></div>
