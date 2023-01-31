0541a6f9b4f63c61381f03313ff7cbdf25a206ea
Mehul Gohil <mehul.gohil@get10up.com>
2023-01-31 16:56:50 +0530

    improve validation for podcast fields

--- includes/datatypes.php
+++ includes/datatypes.php
@@ -852,33 +852,38 @@ function get_podcasting_language_options() {
 /**
  * Validate Podcast Taxonomy Fields.
  *
- * @param string $term
- * @param [type] $taxonomy
- * @param [type] $args
- * @return void
+ * @param string $term     Term.
+ * @param string $taxonomy Taxonomy Name.
+ * @param array  $args     List of arguments.
+ *
+ * @return string
  */
 function validate_taxonomy_fields( $term, $taxonomy, $args ) {
 	// Bailout, if not the podcasts taxonomy.
 	if ( 'podcasting_podcasts' !== $taxonomy ) {
-		return;
+		return $term;
+	}
+
+	if ( empty( trim( $args['tag-name'] ) ) ) {
+		return new \WP_Error( 'empty_term_name', __( 'A podcast name is required..' ) );
 	}
 
 	// Validate Empty Podcast Author Name.
 	if ( empty( trim( $args['podcasting_talent_name'] ) ) ) {
-		return new \WP_Error( 'empty_term_talent_name', __( 'A Podcast Artist / Author Name is required.' ) );
+		return new \WP_Error( 'empty_term_talent_name', __( 'A podcast artist or author name is required.' ) );
 	}
 
 	// Validate Empty Podcast Summary.
 	if ( empty( trim( $args['podcasting_summary'] ) ) ) {
-		return new \WP_Error( 'empty_term_summary', __( 'A Podcast summary is required.' ) );
+		return new \WP_Error( 'empty_term_summary', __( 'A podcast summary is required.' ) );
 	}
 
 	// Validate Podcast Image.
 	if ( empty( trim( $args['podcasting_image'] ) ) ) {
-		return new \WP_Error( 'empty_term_cover_image', __( 'A Podcast cover image is required.' ) );
+		return new \WP_Error( 'empty_term_cover_image', __( 'A podcast cover image is required.' ) );
 	}
 
-
+	return $term;
 }
 
 add_filter( 'pre_insert_term', __NAMESPACE__ . '\validate_taxonomy_fields', 10, 3 );
