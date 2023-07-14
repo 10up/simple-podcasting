5fc43efc6df63034a754a67882fc72acd82c4794
Kirtan Gajjar <8456197+kirtangajjar@users.noreply.github.com>
2023-07-14 21:53:11 +0530

    Update templates/transcript.php
    Co-authored-by: Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>

--- templates/transcript.php
+++ templates/transcript.php
@@ -13,7 +13,12 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php esc_html_e( 'Transcript', 'simple-podcasting' ); ?> - <?php echo esc_html( get_the_title() ); ?></title>
+	<title><?php
+	printf(
+		esc_html__( 'Transcript - %s', 'simple-podcasting' ),
+		get_the_title()
+	);
+	?></title>
 </head>
 <body>
 <?php
