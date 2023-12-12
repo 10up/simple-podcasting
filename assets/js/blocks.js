29e5b7884d140a1d0aeaa420360292633de1fa2b
barneyjeffries <barney@brightwhitespace.co.uk>
2023-12-12 15:56:19 +0000

    added variation and custom query

--- assets/js/blocks.js
+++ assets/js/blocks.js
@@ -2,7 +2,7 @@
  * Internal block libraries
  */
 import { __ } from '@wordpress/i18n';
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType, registerBlockVariation } from '@wordpress/blocks';
 
 // Split the Edit component out.
 import Edit from './edit';
@@ -105,3 +105,39 @@ export default registerBlockType(
 		},
 	},
 );
+
+const VARIATION_NAME = 'podcasting/latest-episode';
+
+registerBlockVariation('core/query', {
+	name: VARIATION_NAME,
+	title: 'Latest Podcast Episode',
+	description: 'Displays the latest podcast episode.',
+	isActive: ['simple-podcasting'],
+	icon: 'microphone',
+	attributes: {
+		namespace: VARIATION_NAME,
+		query: {
+			postType: 'post',
+			podcastingQuery: 'not_empty',
+		},
+	},
+	allowedControls: [ ],
+	scope: [ 'inserter' ],
+	innerBlocks: [
+		[
+			'core/post-template',
+			{},
+			[ [
+				'core/group',
+				{ className: 'podcasting-latest-episode' },
+				[
+					[ 'core/post-featured-image' ],
+					[ 'core/group', { className: 'podcasting-latest-episode__content' }, [
+						[ 'core/post-title' ], [ 'core/post-date' ], [ 'core/post-excerpt' ]
+					] ],
+				]
+			] ],
+		],
+		[ 'core/query-no-results' ],
+	],
+});
