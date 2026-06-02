6186772264a18755db66747a113c0e7faef4b8f9
Jeffrey Paul <jeffpaul@hotmail.com>
2026-06-02 10:50:30 -0500

    Update required WordPress version to 6.8

--- simple-podcasting.php
+++ simple-podcasting.php
@@ -4,7 +4,7 @@
  * Plugin URI:        https://github.com/10up/simple-podcasting
  * Description:       Easily set up multiple podcast feeds using built-in WordPress posts. Includes a podcast block for the new WordPress editor.
  * Version:           1.9.1
- * Requires at least: 6.6
+ * Requires at least: 6.8
  * Requires PHP:      7.4
  * Author:            10up
  * Author URI:        http://10up.com/
